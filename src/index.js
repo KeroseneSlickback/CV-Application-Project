8de9142f9aed69bc6314aed6fb6029b0c03b7e17
Will Spaur <mitchellspaur@gmail.com>
2021-06-10 14:01:39 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.module.css';
+import App from './App';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+
+{/*
+
+	CV Application... application
+
+	Should include:
+
+	- General info: name, email, phone number
+	- Educational experience: School name, title of study, date of study
+	- Practical experience: company name, position title, main tasks of job, date from and until
+	- Submit button
+	- Display info after submission
+	 - with edit button that formats those sections to edit in-line
+	
+
+*/}
